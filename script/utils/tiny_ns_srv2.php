3e5ebed50bb3956ae91866f6598f2668c312ebc5
vincent <tongwensong@gmail.com>
2017-03-10 17:16:25 +0800

    adjust protocol

--- script/utils/tiny_ns_srv2.php
+++ script/utils/tiny_ns_srv2.php
@@ -4,9 +4,6 @@ require_once(
     dirname(dirname(__FILE__))
     .'/init_env.php'
 );
-require_once(
-    Da\Sys_App::lib_path("CNsHead.class.php")
-);
 $opt = getopt('h::p::', ['2json', '2sp']);
 $host = isset($opt['h']) ? $opt['h'] : '0.0.0.0';
 $port = isset($opt['p']) ? $opt['p'] : '8765';
@@ -19,9 +16,7 @@ $socket = stream_socket_server(
 $process = function () use ($socket, $opt)
 {
     $conn = stream_socket_accept($socket, 30);
-    $ns = new NsHead();
-    $data = $ns->nshead_read($conn);
-    $result = mc_pack_pack2array($data['buf']);
+    $result = fgets($conn);
     isset($opt['2json'])
     ? print(json_encode($result))
     : (
@@ -31,9 +26,7 @@ $process = function () use ($socket, $opt)
     );
     echo PHP_EOL;
     $data = ['errno'=>0, 'data'=> 'ok'];
-    $body = mc_pack_array2pack($data);
-    $hdr  = array('body_len' => strlen($body));
-    $ns->nshead_write($conn, $hdr, $body);
+    fwrite($conn, json_encode($data));
 };
 
 echo 'Listening at port: '.$port.PHP_EOL;
